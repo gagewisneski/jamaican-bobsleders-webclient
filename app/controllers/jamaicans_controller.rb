a2cc7356ea471b53e4cf3d0f924c856179ab5692
gagewisneski <gagewisneski@gmail.com>
2016-12-14 19:15:30 -0800

    Added DotENV gem

--- app/controllers/jamaicans_controller.rb
+++ app/controllers/jamaicans_controller.rb
@@ -1,35 +1,35 @@
 class JamaicansController < ApplicationController
 
   def index
-    @jamaicans = Unirest.get("http://localhost:3001/api/v2/jamaicans").body
+    @jamaicans = Unirest.get("#{ENV['API_URL']}/jamaicans").body
   end
 
   def show
-    @jamaican = Unirest.get("http://localhost:3001/api/v2/jamaicans/#{params[:id]}.json").body
+    @jamaican = Unirest.get("#{ENV['API_URL']}/jamaicans/#{params[:id]}.json").body
   end
 
   def new
   end
 
   def create
-    @jamaican = Unirest.post("http://localhost:3001/api/v2/jamaicans", parameters: {first_name: params[:first_name], last_name: params[:last_name], position: params[:position]}, headers:{"Accept" => "application/json"}).body
+    @jamaican = Unirest.post("#{ENV['API_URL']}/jamaicans", parameters: {first_name: params[:first_name], last_name: params[:last_name], position: params[:position]}, headers:{"Accept" => "application/json"}).body
 
     redirect_to "/jamaicans/#{@jamaican['id']}"
     # redirect_to jamaican_path(@jamaican["id"])
   end
 
   def edit
-    @jamaican = Unirest.get("http://localhost:3001/api/v2/jamaicans/#{params[:id]}.json").body
+    @jamaican = Unirest.get("#{ENV['API_URL']}/jamaicans/#{params[:id]}.json").body
   end
 
   def update
-    @jamaican = Unirest.patch("http://localhost:3001/api/v2/jamaicans/#{params[:id]}", parameters: {first_name: params[:first_name], last_name: params[:last_name], position: params[:position]}, headers:{"Accept" => "application/json"}).body
+    @jamaican = Unirest.patch("#{ENV['API_URL']}/jamaicans/#{params[:id]}", parameters: {first_name: params[:first_name], last_name: params[:last_name], position: params[:position]}, headers:{"Accept" => "application/json"}).body
 
     redirect_to "/jamaicans/#{@jamaican['id']}"
   end
 
   def destroy
-    @jamaican = Unirest.delete("http://localhost:3001/api/v2/jamaicans/#{params[:id]}")
-    redirect_to "/jamaicans/1"
+    @jamaican = Unirest.delete("#{ENV['API_URL']}/jamaicans/#{params[:id]}")
+    redirect_to "/jamaicans"
   end
 end
